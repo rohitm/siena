6eb578045efb817ec0c5a2eec8ab536a20e35053
Rohit Manohar <rohit.manohar@gmail.com>
2017-08-22 17:01:26 -0400

    Cache server working

---
+++ src/lib/get-bittrex-market-history.js
@@ -0,0 +1,31 @@
+const request = require('request');
+const logger = require('cli-logger');
+
+const log = logger({ level: logger.INFO });
+
+const getBittrexMarketHistory = market => new Promise(async (resolveHistory, rejectHistory) => request(`https://bittrex.com/api/v1.1/public/getmarkethistory?market=${market}`, (error, response, body) => {
+  if (error) {
+    return rejectHistory(error);
+  }
+
+  if (response.body === undefined) {
+    return rejectHistory(new Error('Empty body'));
+  }
+
+  let jsonBody;
+  try {
+    jsonBody = JSON.parse(response.body);
+  } catch (jsonParseError) {
+    return rejectHistory(jsonParseError);
+  }
+
+  if (jsonBody.success !== true) {
+    log.error(`getBittrexMarketHistory: Non success response ${JSON.stringify(body)}, market=${market}`);
+    return rejectHistory(new Error(body.message || 'Unknown error'));
+  }
+
+  return resolveHistory(jsonBody.result);
+}));
+
+
+module.exports = getBittrexMarketHistory;
---
+++ src/market-history-cache.js
@@ -0,0 +1,79 @@
+const getBittrexMarketHistory = require('./lib/get-bittrex-market-history');
+const _ = require('lodash');
+const redis = require('redis');
+const logger = require('cli-logger');
+const bluebird = require('bluebird');
+const express = require('express');
+const config = require('config');
+
+const log = logger({ level: logger.INFO });
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const redisClient = redis.createClient();
+redisClient.on('error', redisError => log.error(redisError));
+
+const cacheMarketHistory = (market, periodInMilliseconds) => new Promise(
+  async (resolveCache, rejectCache) => {
+    try {
+      // Get the one hour moving average from bittrex
+      let history = await getBittrexMarketHistory(market);
+
+      // Select unique transaction Ids
+      history = _.uniqBy(history, 'Id');
+
+      // Sort it by the transaction time
+      history = _.sortBy(history, [
+        object => Date.parse(((object.TimeStamp.slice(-1) !== 'Z') ? `${object.TimeStamp}Z` : object.TimeStamp)),
+      ]).reverse();
+
+      // TODO: Cache them in redis
+      const redisZAddParams = history.reduce((acc, object) => {
+        const timestamp = Date.parse(((object.TimeStamp.slice(-1) !== 'Z') ? `${object.TimeStamp}Z` : object.TimeStamp));
+        return [...acc, timestamp, JSON.stringify(object)];
+      }, [market]);
+
+      // Add to the cache
+      await redisClient.zadd(redisZAddParams);
+      log.info(`cacheMarketHistory: ${history.length} transactions cached`);
+
+      // Clean up the cache
+      const cacheLowerLimitTimestamp = new Date().getTime() - periodInMilliseconds;
+      log.info(`cacheMarketHistory: Cleaning cache with redis code : 'ZREMRANGEBYSCORE ${market} -inf ${cacheLowerLimitTimestamp}'`);
+      await redisClient.zremrangebyscore([market, '-inf', cacheLowerLimitTimestamp]);
+
+      resolveCache(true);
+    } catch (cacheMarketHistoryError) {
+      log.error(`cacheMarketHistory: ${cacheMarketHistoryError}`);
+      rejectCache(cacheMarketHistoryError);
+    }
+  });
+
+const getMarketHistory = market => new Promise(async (resolveGet, rejectGet) => redisClient.zrange([market, '0', '-1'], (error, data) => {
+  if (error) {
+    return rejectGet(error);
+  }
+
+  // Convert the objects into strings
+  return resolveGet(data.map(JSON.parse));
+}));
+
+// Cache Every 15 mins
+log.info(`marketHistoryCache: Polling bittrex for history every ${config.get('pollIntervalMinutes')} minutes`);
+setInterval(() => cacheMarketHistory('USDT-BTC', (3600000 * 12)), config.get('pollIntervalMinutes') * 60 * 1000);
+
+const app = express();
+
+app.get('/getMarketHistory', async (req, res) => {
+  res.set('Content-Type', 'application/json');
+
+  if (!_.has(req, 'query.market')) {
+    return res.send({ success: false, message: 'need market' });
+  }
+
+  return res.send({ success: true, result: await getMarketHistory(req.query.market) });
+});
+
+app.listen(config.get('cacheServerPort'));
+log.info(`marketHistoryCache: Server started on port ${config.get('cacheServerPort')}`);
