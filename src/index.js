6a150ac63f0f1e159b7cdcaa9b5b7f95ef45e94f
Rohit Manohar <rohit.manohar@gmail.com>
2017-08-23 00:41:34 -0400

    basic recommendation done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,48 @@
 const movingAverage = require('./lib/moving-average');
+const getTicker = require('./lib/get-ticker');
+const logger = require('cli-logger');
+const _ = require('lodash');
 
-(async () => {
-  // Get the one hour moving average from bittrex
-  const toTimestamp = new Date().getTime();
-  const fromTimestamp = toTimestamp - (3600000); // One hour
-  const oneHourMovingAverage = await movingAverage('USDT-ETH', fromTimestamp, toTimestamp);
-  console.log(oneHourMovingAverage);
+const log = logger({ level: logger.INFO });
 
-  // 24 hourmoving average
-  const fromTimestamp24 = toTimestamp - (3600000 * 24); // 24 hours
-  const twelveMovingAverage = await movingAverage('USDT-ETH', fromTimestamp24, toTimestamp, 'bittrexCache');
-  console.log(twelveMovingAverage);
-})();
+let previousRecommendation;
+
+const poll = market => new Promise(async (resolvePoll, rejectPoll) => {
+  try {
+    // Get the one hour moving average from bittrex
+    // & 24 hourmoving average from the cache or bittrex
+    const toTimestamp = new Date().getTime();
+    const fromTimestamp = toTimestamp - (3600000); // One hour
+    const fromTimestamp24 = toTimestamp - (3600000 * 24); // 24 hours
+
+    const tasks = [
+      getTicker(market),
+      movingAverage(market, fromTimestamp, toTimestamp),
+      movingAverage(market, fromTimestamp24, toTimestamp, 'bittrexCache'),
+    ];
+
+    const [ticker, movingAverageOne, movingAverage24] = await Promise.all(tasks);
+
+    // TODO : Publish to events or rules queue
+    const recommendation = {};
+    if (movingAverageOne > movingAverage24) {
+      recommendation.action = 'BUY';
+      recommendation.buyPrice = ticker.Ask;
+    } else {
+      recommendation.action = 'SELL';
+      recommendation.sellPrice = ticker.Bid;
+    }
+
+    if (_.isEqual(recommendation, previousRecommendation)) {
+      return;
+    }
+
+    previousRecommendation = _.cloneDeep(recommendation);
+    log.info(`poll, recommendation : ${recommendation.action} currency in ${market} at price ${(recommendation.buyPrice || recommendation.sellPrice)}`);
+  } catch (pollError) {
+    log.error(`poll, error: ${pollError}`);
+    rejectPoll(pollError);
+  }
+});
+
+setInterval(() => poll('USDT-ETH'), 1000);
---
+++ src/lib/get-ticker.js
@@ -0,0 +1,37 @@
+const request = require('request');
+const config = require('config');
+
+const getTicker = (market, source = 'bittrex') => new Promise(async (resolveGetTicker, rejectGetTicker) => {
+  const url = `${config.get(`${source}.gettickerurl`)}?market=${market}`;
+
+  try {
+    const ticker = await (new Promise((resolve, reject) => request(url, (error, response, body) => {
+      if (error) {
+        return reject(error);
+      }
+
+      if (response.body === undefined) {
+        return reject(new Error('Empty body'));
+      }
+
+      let jsonBody;
+      try {
+        jsonBody = JSON.parse(response.body);
+      } catch (jsonParseError) {
+        return reject(jsonParseError);
+      }
+
+      if (jsonBody.success !== true) {
+        return reject(new Error(body.message || 'Unknown error'));
+      }
+
+      return resolve(jsonBody.result);
+    })));
+
+    return resolveGetTicker(ticker);
+  } catch (error) {
+    return rejectGetTicker(error);
+  }
+});
+
+module.exports = getTicker;
