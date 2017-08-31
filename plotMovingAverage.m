98cac06074704fbc7d60ec4f10686ae5492bc04c
Rohit Manohar <rohit.manohar@gmail.com>
2017-08-31 16:42:19 -0400

    moving average changed to 6 to 1 hours

--- plotMovingAverage.m
+++ plotMovingAverage.m
@@ -4,15 +4,16 @@ movingAverages2 = [];
 
 for endTime = seconds
   % Short moving average
-  startTime = endTime - 1125000;
+  startTime = endTime - 3600000;
   movingAverages1(end+1) = mean(tradeHistory(find (tradeHistory(:,1) >= startTime & tradeHistory(:,1) <= endTime), 2));
 
   % Long moving average
-  startTime = endTime - 18000000;
+  startTime = endTime - 21600000;
   movingAverages2(end+1) = mean(tradeHistory(find (tradeHistory(:,1) >= startTime & tradeHistory(:,1) <= endTime), 2));
 end
 
 plot(seconds, movingAverages1, '-r.')
 hold on
   plot(seconds, movingAverages2, '-b.')
+  plot(tradeHistory(:,1),tradeHistory(:,2),'-k.')
 hold off
