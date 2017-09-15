332a1eddc2a96477f9c96ca9a1d706ac88091e75
Rohit Manohar <rohit.manohar@gmail.com>
2017-09-14 20:22:25 -0400

    Plotting three moving averages

--- plotMovingAverage.m
+++ plotMovingAverage.m
@@ -11,17 +11,22 @@ buyOrSell = buySellPoints(:,3);
 
 for endTime = seconds
   % Short moving average
-  startTime = endTime - 3600000;
+  startTime = endTime - 1800000;
   movingAverages1(end+1) = mean(tradeHistory(find (tradeHistory(:,1) >= startTime & tradeHistory(:,1) <= endTime), 2));
 
   % Long moving average
-  startTime = endTime - 21600000;
+  startTime = endTime - 18000000;
   movingAverages2(end+1) = mean(tradeHistory(find (tradeHistory(:,1) >= startTime & tradeHistory(:,1) <= endTime), 2));
+
+  % Long moving average
+  startTime = endTime - 46800000;
+  movingAverages3(end+1) = mean(tradeHistory(find (tradeHistory(:,1) >= startTime & tradeHistory(:,1) <= endTime), 2));
 end
 
 plot(seconds, movingAverages1, '-r.');
 hold on
   plot(seconds, movingAverages2, '-b.');
+  plot(seconds, movingAverages3, '-g.');
   plot(tradeHistory(:,1),tradeHistory(:,2),'-k.');
 
   % Plot sell points
