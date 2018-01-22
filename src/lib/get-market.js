34c95836b4ab1a474fd09b7b74d42e9cc557fb6d
Rohit Manohar <rohit.manohar@gmail.com>
2018-01-22 17:07:21 -0500

    get market api call added to get the minimum trade size

---
+++ src/lib/get-market.js
@@ -0,0 +1,43 @@
+const request = require('request');
+const config = require('config');
+
+const getMarket = (baseCurrency, securityCurrency, source = 'bittrex') => new Promise(async (resolveGetMarket, rejectGetMarket) => {
+  const url = `${config.get(`${source}.getmarketsurl`)}`;
+
+  try {
+    const markets = await (new Promise((resolve, reject) =>
+      request(url, (error, response) => {
+        if (error) {
+          return reject(error);
+        }
+
+        if (response.body === undefined) {
+          return reject(new Error('Empty body'));
+        }
+
+        let jsonBody;
+        try {
+          jsonBody = JSON.parse(response.body);
+        } catch (jsonParseError) {
+          return reject(jsonParseError);
+        }
+
+        if (jsonBody.success !== true) {
+          return reject(new Error(jsonBody.message || 'Unknown error'));
+        }
+
+        return resolve(jsonBody.result);
+      })));
+
+    if (markets.length === 0) {
+      return rejectGetMarket(new Error('Markets not returned'));
+    }
+
+    const market = markets.filter(market => (market.BaseCurrency === baseCurrency && market.MarketCurrency === securityCurrency));
+    return resolveGetMarket(market[0]);
+  } catch (error) {
+    return rejectGetMarket(error);
+  }
+});
+
+module.exports = getMarket;
