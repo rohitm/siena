436ffd903b1ced2550dfba2b2f7f5137d0d04a7e
Rohit Manohar <rohit.manohar@gmail.com>
2018-02-05 17:49:53 -0500

    Nav script logs bittrex balance

--- src/nav.js
+++ src/nav.js
@@ -1,10 +1,16 @@
 const config = require('config');
 const bunyan = require('bunyan');
 const getNetAssetValue = require('./lib/get-net-asset-value');
+const getBalances = require('./lib/get-balances');
 
 const log = bunyan.createLogger({ name: 'nav' });
 
 (async () => {
-  const nav = await getNetAssetValue();
+  const tasks = [
+    getNetAssetValue(),
+    getBalances(),
+  ];
+  const [nav, bittrexBalances] = await Promise.all(tasks);
+  log.info(bittrexBalances);
   log.info(`The current net asset value of your bittrex account trading with the ${config.get('bittrexMarket')} market is $${nav} USD`);
 })();
